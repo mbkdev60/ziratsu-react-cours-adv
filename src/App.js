b45fa6daf63256db08089767d2a89602558fc19f
moez <bkmoez2410@gmail.com>
2023-03-01 18:16:25 +0100

    add video module

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import Video from "./Video.mp4";
+// import Video from "./Video.mp4";
 
 function App() {
-  // const [timer, setTimer] = useState(1);
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState([1, 2, 3]);
 
-  // useEffect(() => {
-  //   const intervalID = setInterval(() => {
-  //     setTimer(timer => timer + 1);
-  //   }, 1000)
-
-  //   return () => {
-  //     clearInterval(intervalID)
-  //   }
-  // }, [])
   // useEffect se lance lors du 1er chargement
   //le tableau pour le state à surveiller
 
-  const ref = useRef([]);
-
-  useEffect(() => {
-    console.log(ref);
-  }, []);
-
-  const addToRef = (el) => {
-    // console.log(el);
-    if (el && !ref.current.includes(el)) {
-      ref.current.push(el);
-    }
-  };
-
   const toggleFunc = () => {
-    setToggle(!toggle);
+    const newArr = [...toggle];
+    newArr.push(4);
+
+    setToggle(newArr);
+    // setToggle(!toggle);
   };
   return (
     <div className="App">
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
+      <h1>{toggle}</h1>
       <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
