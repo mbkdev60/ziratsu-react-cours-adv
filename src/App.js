583f43a57dea072162fbc1c75c6bafbefb000419
moez <bkmoez2410@gmail.com>
2023-03-03 22:22:09 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import { useState, useEffect, useMemo } from "react";
 import "./App.css";
-import Content from "./Content";
-// import Video from "./Video.mp4";
+import Modal from "./Components/Modal/Modal";
 
 function App() {
-  const [toggle, setToggle] = useState([1, 2, 3]);
-
   // useEffect se lance lors du 1er chargement
   //le tableau pour le state à surveiller
 
-  const toggleFunc = () => {
-    const newArr = [...toggle];
-    newArr.push(4);
-
-    setToggle(newArr);
-    // setToggle(!toggle);
-  };
-
-  const tableau = useMemo(() => {
-    return [1,2,3,4]
-  }, [])
-
   return (
-    <div className="App">
-      <Content num={tableau} />
-      <button onClick={toggleFunc}>Toggle2</button>
-    </div>
+    <>
+      <Modal />
+    </>
   );
 }
 
